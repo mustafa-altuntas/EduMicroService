969449568cc0dcb6818a6417e4a67a03dec53ca5
mustafa altuntaş <altun_tas@outlook.com>
2025-04-24 15:27:41 +0300

    Add AddBasketItemCommand and its validator
    Introduces the `AddBasketItemCommand` record to manage course details for basket items, implementing the `IRequestByServiceResult` interface. Adds `AddBasketItemCommandValidator` to enforce validation rules, ensuring `CourseId` and `CourseName` are required and `CoursePrice` is greater than zero.

---
+++ EduMicroService.Basket.Api/Features/Baskets/AddBasketItem/AddBasketItemCommand.cs
@@ -0,0 +1,7 @@
+﻿using EduMicroService.Shared;
+
+namespace EduMicroService.Basket.Api.Features.Baskets.AddBasketItem
+{
+    public record AddBasketItemCommand(Guid CourseId, string CourseName, decimal CoursePrice, string? ImageUrl)
+        : IRequestByServiceResult;
+}
