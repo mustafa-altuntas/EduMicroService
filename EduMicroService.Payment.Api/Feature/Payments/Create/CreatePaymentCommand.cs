0b7cc2fa7ac58c85877d3d3fbb3f4c19881a12d5
mustafa altuntaş <altun_tas@outlook.com>
2025-06-26 17:18:34 +0300

    Remove folder reference and add CreatePaymentCommand
    Updated `EduMicroService.Payment.Api.csproj` to remove the folder reference for `Feature\Payments\Create`. Added a new record `CreatePaymentCommand` in `CreatePaymentCommand.cs` to encapsulate payment details, implementing the `IRequestByServiceResult<Guid>` interface for command handling.

---
+++ EduMicroService.Payment.Api/Feature/Payments/Create/CreatePaymentCommand.cs
@@ -0,0 +1,12 @@
+﻿using EduMicroService.Shared;
+
+namespace EduMicroService.Payment.Api.Feature.Payments.Create
+{
+    public record CreatePaymentCommand(
+        string OrderCode,
+        string CardNumber,
+        string CardHolderName,
+        string CardExpirationDate,
+        string CardSecurityNumber,
+        decimal Amount) : IRequestByServiceResult<Guid>;
+}
